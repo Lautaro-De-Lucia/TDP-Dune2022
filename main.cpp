694b9f1077fbd9b3f83aec6c5459cb8429172956
Manuel Collazo <collazo.manuel.28@gmail.com>
2022-06-05 20:30:39 -0300

    fix main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include "common_utils.h"
 #include <SDL2pp/SDL2pp.hh>
 
+#include "src/ui/vistas.h"
+#include <QApplication>
+#include <QtConcurrent/QtConcurrent>
+
+
 #define INIT_SPICE 5000
 #define INIT_CSPICE 5000
 #define INIT_ENERGY 3000
@@ -12,7 +17,7 @@
 #define MAP_DIM_X 80
 #define MAP_DIM_Y 45
 
-std::vector<std::vector<cell_t>> generate_random_map (){
+std::vector<std::vector<cell_t>> generate_random_map () {
     std::vector<std::vector<cell_t>> map_cells(MAP_DIM_X);
     for (size_t i = 0 ; i < MAP_DIM_X ; i++) {
         map_cells[i].resize(MAP_DIM_Y);
@@ -25,7 +30,7 @@ std::vector<std::vector<cell_t>> generate_random_map (){
     return map_cells;
 }
 
-int main (void){
+void run_sdl() {
     SDL2pp::Window game_window("Dune II",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,SCREEN_WIDTH, SCREEN_HEIGHT,SDL_WINDOW_ALWAYS_ON_TOP);
     SDL2pp::Renderer game_renderer(game_window, -1, SDL_RENDERER_ACCELERATED);
     Board board(45,25);
@@ -33,4 +38,26 @@ int main (void){
     std::vector<std::vector<cell_t>> cells = generate_random_map();
     CPlayer(game_window,game_renderer,INIT_SPICE,INIT_CSPICE,INIT_ENERGY,INIT_CENERGY,cells);
     server.run();
+};
+
+int main(int argc, char *argv[]) {
+
+
+    /*
+    QApplication a(argc, argv);
+    a.setWindowIcon(QIcon("./src/resources/img/icon.png"));
+    MainWindow w;
+    w.show();
+    LobbyWindow l;
+
+    QObject::connect(&w, &MainWindow::jugar, &w, &QMainWindow::close);
+    QObject::connect(&w, &MainWindow::jugar, &l, &QMainWindow::show);
+    QObject::connect(&l, &LobbyWindow::jugar, &l, [=]() {QtConcurrent::run(run_sdl);});
+
+    return a.exec();
+    */
+
+    run_sdl();
+
 }
+
