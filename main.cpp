3b54d8a36f37ff0c664b898df3a484e3b63eff06
Lautaro De Lucia <ldelucia@fi.uba.ar>
2022-06-13 10:28:56 -0300

    Primer intento de refactorización

--- main.cpp
+++ main.cpp
@@ -26,7 +26,6 @@ std::vector<std::vector<cell_t>> generate_random_map () {
     for (size_t i = 0 ; i < MAP_DIM_X ; i++) {
         map_cells[i].resize(MAP_DIM_Y);
     }
-    //  
     for (size_t j = 0 ; j < MAP_DIM_Y ; j++) {
         for (size_t i = 0 ; i < MAP_DIM_X ; i++) {
                 map_cells[i][j] = ROCK;
