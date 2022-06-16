dbef098c3f1778a8656ca9d9d9920fdfd81f19dd
Manuel Collazo <collazo.manuel.28@gmail.com>
2022-06-16 14:59:40 -0300

    game state protocol headers added

--- include/client_player.h
+++ include/client_player.h
@@ -15,39 +15,39 @@
 
 class CPlayer {
  private:
-   size_t spice;
-   size_t c_spice;
-   size_t c_energy;
-   int energy;
-   double efficiency;
-   std::unique_ptr<SDL2pp::Texture> user_message;
-   std::map<int,std::unique_ptr<CSelectable>> elements;
-   MouseHandler mouse;
-   CPrinter printer;
-   GameMap map;
-   GameHud hud;
-   SDL2pp::Window& game_window;
-   SDL2pp::Renderer& game_renderer;
-   Camera& camera;
-   bool is_holding_building;
-   int building_held;
+    size_t spice;
+    size_t c_spice;
+    size_t c_energy;
+    int energy;
+    double efficiency;
+    std::unique_ptr<SDL2pp::Texture> user_message;
+    std::map<int, std::unique_ptr<CSelectable>> elements;
+    MouseHandler mouse;
+    CPrinter printer;
+    GameMap map;
+    GameHud hud;
+    SDL2pp::Window& game_window;
+    SDL2pp::Renderer& game_renderer;
+    Camera& camera;
+    bool is_holding_building;
+    int building_held;
 
 
  public:
-   CPlayer(Camera& cam, SDL2pp::Window& window,SDL2pp::Renderer& renderer,size_t spice, size_t c_spice, int energy, size_t c_energy, std::vector<std::vector<cell_t>>& map_data);
-   void renderMap();
-   void renderHud();
-   void addElement(unit_t type,State& desc);
-   void addElement(building_t type,State& desc);
-   void update(std::vector<State>& server_data, int spice, int energy);
-   void clientUpdate(std::vector<int>& mouse_event);
+    CPlayer(Camera& cam, SDL2pp::Window& window,SDL2pp::Renderer& renderer, size_t spice, size_t c_spice, int energy, size_t c_energy, std::vector<std::vector<cell_t>>& map_data);
+    void renderMap();
+    void renderHud();
+    void addElement(unit_t type,State& desc);
+    void addElement(building_t type,State& desc);
+    void update(std::vector<State>& server_data, int spice, int energy);
+    void clientUpdate(std::vector<int>& mouse_event);
 
-   void print(std::string toprint, std::string fontpath, int x, int y, int size ,SDL_Color color,size_t time);
-   
-   void addUnitButton(std::string& IMG_PATH, int& x, int& y, int& id);
-   void addBuildButton(std::string& IMG_PATH, int& x, int& y, int& id);
-   int checkBuild(int& x, int& y);
-   int checkUnit(int& x, int& );
-   bool checkHud(int& x, int& y);
-   hud_button_t checkBtn(int& x, int& y);
+    void print(std::string toprint, std::string fontpath, int x, int y, int size ,SDL_Color color,size_t time);
+    
+    void addUnitButton(std::string& IMG_PATH, int& x, int& y, int& id);
+    void addBuildButton(std::string& IMG_PATH, int& x, int& y, int& id);
+    int checkBuild(int& x, int& y);
+    int checkUnit(int& x, int& );
+    bool checkHud(int& x, int& y);
+    hud_button_t checkBtn(int& x, int& y);
 };
--- include/player.h
+++ include/player.h
@@ -20,31 +20,31 @@
 
 class Player {
  private:
-    int ID;
-    int spice;
-    int c_spice;
-    int energy;
-    int c_energy;
-    player_t faction;
-    double efficiency;
-    CPlayer& cplayer;
-    std::map<int,std::unique_ptr<Selectable>> elements;
-    Board board;
-    std::queue<std::vector<int>> mouse_events;
-    std::vector<State> states;
-    bool place(Building& building,Position position);
-    bool place(Refinery& refinery,Position& position);
-    bool place(Unit& unit,std::vector<Position> positions);
+   int ID;
+   int spice;
+   int c_spice;
+   int energy;
+   int c_energy;
+   player_t faction;
+   double efficiency;
+   CPlayer& cplayer;
+   std::map<int,std::unique_ptr<Selectable>> elements;
+   Board board;
+   std::queue<std::vector<int>> mouse_events;
+   std::vector<State> states;
+   bool place(Building& building,Position position);
+   bool place(Refinery& refinery,Position& position);
+   bool place(Unit& unit,std::vector<Position> positions);
  public:    
-    Player (player_t faction ,int spice, int c_spice, int energy, int c_energy,std::vector<std::vector<cell_t>> cell_types,CPlayer& client_player);
-    void print();
-    void run(); // This should receive the socket in the future
-    void update();
-    void createBuilding(int type, int pos_x, int pos_y);
-    void createUnit(int type);
-    void handleLeftClick(int x, int y);
-    void handleRightClick(int x, int y);
-    void handleSelection(int xmin, int xmax, int ymin, int ymax);
-    void handleIdle();
-    void reportStateToClient();
+   Player (player_t faction ,int spice, int c_spice, int energy, int c_energy,std::vector<std::vector<cell_t>> cell_types,CPlayer& client_player);
+   void print();
+   void run(); // This should receive the socket in the future
+   void update();
+   void createBuilding(int type, int pos_x, int pos_y);
+   void createUnit(int type);
+   void handleLeftClick(int x, int y);
+   void handleRightClick(int x, int y);
+   void handleSelection(int xmin, int xmax, int ymin, int ymax);
+   void handleIdle();
+   void reportStateToClient();
 };
